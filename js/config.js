7eea51be1db5022faf43ccd580755974f43ffbe6
nico <niky.triano@gmail.com>
2026-09-12 21:05:23 +0200

    csc

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
   API_URL: 'https://script.google.com/macros/library/d/1JRUN-ldDMsNdzs3x-dD4w5PDHleTaQr5z9gtsBZaya7BWHWnDdbYKcpl/1',
-  MOCK_BACKEND: true, 
+  MOCK_BACKEND: false, 
   WEATHER_API_KEY: 'c5b6238dd596172bbecec04ca942a6cc' 
 };
