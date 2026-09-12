fa79b1fcee0053252a307d0399ff8b4051005b47
nico <niky.triano@gmail.com>
2026-09-12 18:55:12 +0200

    dc

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
   API_URL: 'https://script.google.com/macros/s/AKfycbwxwtM8Ts0frRnzzDF4L33vCq3qqK8ydxRgGSKdnwq4zkPLvbO6izxvbHqMPSIt1nfS/exec',
   MOCK_BACKEND: true, 
-  WEATHER_API_KEY: 'INSERISCI_API_KEY_OPENWEATHERMAP' 
+  WEATHER_API_KEY: 'c5b6238dd596172bbecec04ca942a6cc' 
 };
