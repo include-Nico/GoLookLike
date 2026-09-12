77511ffe7599af1bacc57fe1eb82cc1eb7d14574
nico <niky.triano@gmail.com>
2026-09-12 23:23:09 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-  API_URL: 'https://script.google.com/macros/s/AKfycbwxwtM8Ts0frRnzzDF4L33vCq3qqK8ydxRgGSKdnwq4zkPLvbO6izxvbHqMPSIt1nfS/exec',
+  API_URL: 'https://script.google.com/macros/s/AKfycbwwdagdgVisihs5MH7jVmHPR4Pc187YMGwR3i6ZowO152jIxxgOwww2JXpX232BgTmlLA/exec',
   MOCK_BACKEND: false, 
   WEATHER_API_KEY: 'c5b6238dd596172bbecec04ca942a6cc' 
 };
